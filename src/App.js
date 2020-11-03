38ea87fc8f56ecd10a15a2329014abed09c6fbcb
Pedro Henrique <pedro.semear@gmail.com>
2020-11-02 23:42:43 -0300

    Estado e imutabilidade

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import Header from './components/Header';
+
+
+/**
+ * conceitos importantes do react
+ * Componente
+ * Propriedade - informação que passa do componente pai para o componente filho
+ * Estado e imutabilidade 
+ */
+
+function App() {
+
+  const [projects, setProjets] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  //useState retona um array com duas posições. Na primeira ele retorna a variavel com seu valor inicial. Na segunda uma funcao para atualizar esse valor
+
+  function handleAddproject() {
+    //projects.push(`Novo Projeto ${Date.now()}`);
+    
+    setProjets([... projects, `Novo Projeto ${Date.now()}`]);
+  }
+
+  return (
+    <>
+      <Header title="Projects"/>	  
+      <ul>
+        {projects.map(project => <li key={project} >{project}</li>)}
+      </ul>
+      <button type="button" onClick={handleAddproject}>Adicionar Projeto</button>
+    </>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import{render} from 'react-dom';
+import App from './App';
 
-//jsx
+//jsx: html dentro do javascript
 
-render(<h1>Hello World!</h1>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
